57f4fe80db17c3c6a28be65b8b811ed202ae4e1a
fbacso <50657103+FerencBacso89@users.noreply.github.com>
2023-08-08 13:12:50 +0200

    add table wich is include services
    Add js code wich is create rows in table and send data to table

--- makePDF.js
+++ makePDF.js
@@ -9,3 +9,26 @@
             };
             html2pdf().from(element).set(opt).save();
         }
+    AddServiceToTable=()=>{
+        const addBtn = document.getElementById("addServiceBtn"); 
+        const tbody = document.querySelector("#servTable tbody");
+
+            const newRow = document.createElement("tr");
+            const serviceInput =document.querySelector(".inputService");
+            
+            const serviceId = serviceInput.querySelector("#sid").value;
+            const serviceName = serviceInput.querySelector("#sname").value;
+            const tax = serviceInput.querySelector("#stax").value;
+            const net = serviceInput.querySelector("#snet").value;
+            const gross = serviceInput.querySelector("#sgross").value; // to be calculate-> net*((tax/100)+1)
+                const values = [serviceId, serviceName, tax, net, gross];
+
+                values.forEach(value => {
+                    const cell = document.createElement("td");
+                    cell.textContent = value;
+                    newRow.appendChild(cell);
+                });
+            // Append the new row to the table body
+        tbody.appendChild(newRow);
+    }
+    
