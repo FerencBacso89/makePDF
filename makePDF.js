558dbd25f4ee737e03212b8d1c6d31e998ccd354
fbacso <50657103+FerencBacso89@users.noreply.github.com>
2023-08-09 15:20:34 +0200

    so much small layout desin part
    customer and company header text
    customer parameters send out to pdf

--- makePDF.js
+++ makePDF.js
@@ -10,32 +10,48 @@
             html2pdf().from(element).set(opt).save();
         }
     AddServiceToTable=()=>{
+         errorMsg.style.display="none";
         const serviceInput =document.querySelector(".inputService");
+        //check the inputfield data is a number or not
          const tax = serviceInput.querySelector("#stax").value.replace(/[^0-9\.]/g, '');
          const net = serviceInput.querySelector("#snet").value.replace(/[^0-9]/g, '');
-            alert(tax);
-        if(tax!= "" && net != ""){
+         // if inputfield not a number tax & net has got an empty string and going to the else. 
+         //After this throw an error message.
+        if(tax!= "" && net != "" ){
             const addBtn = document.getElementById("addServiceBtn");
             const tbody = document.querySelector("#servTable tbody");
-
+                        
                 const newRow = document.createElement("tr");
             if (serviceInput.querySelector("#sname").value != "none"){                    
                 const serviceId = serviceInput.querySelector("#sname").value;
                 const serviceName = document.getElementById("sname").selectedOptions[0].textContent;
             
                 let calcGross = (1+(parseFloat(tax)/100))*parseInt(net);
-                
-                const gross = calcGross; // to be calculate-> net*((tax/100)+1)
+                const gross = Math.round(calcGross)+" EUR"; // to be calculate-> net*((tax/100)+1)
+                // Check Customer data is filled or not
+                let name = document.getElementById("name").value;
+                let email = document.getElementById("email").value;
+                let phone = document.getElementById("phone").value;
+                    if (name!="" && email!="" && phone!=""){
+                            document.getElementById("cusName").innerText=name;
+                            document.getElementById("cusMail").innerText=email;
+                            document.getElementById("cusPhone").innerText=phone;
+                                 
                     const values = [serviceId, serviceName, tax, net, gross];
 
-                    values.forEach(value => {
-                        const cell = document.createElement("td");
-                        cell.textContent = value;
-                        newRow.appendChild(cell);
-                    });
+                        values.forEach(value => {
+                            const cell = document.createElement("td");
+                            cell.textContent = value;
+                            newRow.appendChild(cell);
+                        });
+                    }
+                    else{                       
+                        errorMsg.style.display="block";
+                        errorMsg.textContent="Customer data is not filled. Please fill up your customer's data";
+                    } 
+                   
                 // Append the new row to the table body
-            tbody.appendChild(newRow);
-            errorMsg.style.display="none";
+                tbody.appendChild(newRow);           
             serviceInput.querySelector("#snet").value="";
             } else{
                 // entering an empty data line is not allowed
